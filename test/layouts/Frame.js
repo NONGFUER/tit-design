bb8ba4c2053caa2dc46da65cfa445937372bfcfb
NONGFUER <13852291705@163.com>
2017-03-02 17:56:33 +0800

    添加 全局和头部样式

--- test/layouts/Frame.js
+++ test/layouts/Frame.js
@@ -2,7 +2,8 @@ import React,{Component} from 'react';
 
 import LeftNav from './LeftNav';
 import Header from './Header';
-
+import _index from '../styles/base/_index.scss'
+import _header from '../styles/layout/_header.scss'
 class Frame extends Component{
     render(){
         return (
