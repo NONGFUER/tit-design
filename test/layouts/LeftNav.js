bae1406c244a4f35258d0263dea086193025ff07
NONGFUER <13852291705@163.com>
2017-03-02 15:22:36 +0800

    头部，左侧的demo组件

---
+++ test/layouts/LeftNav.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react';
+
+class LeftNav extends Component{
+    render(){
+        return (
+            <div>
+                <ul>
+                    <li>left1</li>
+                    <li>left2</li>
+                    <li>left3</li>
+                </ul>
+            </div>
+        )};
+}
+
+export default LeftNav;
