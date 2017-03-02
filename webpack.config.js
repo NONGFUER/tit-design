9876c7d02be423b4feef3ecfb6630074e99787bb
NONGFUER <13852291705@163.com>
2017-03-02 17:55:41 +0800

    加载scss

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,8 @@ module.exports = {
                     path.resolve(__dirname, 'src'),
                     path.resolve(__dirname, 'docs'),
                 ]   
-            }
+            },
+             { test: /\.scss$/, loaders: ['style-loader', 'css-loader', 'sass-loader'] },
         ]
     },
     plugins:[
