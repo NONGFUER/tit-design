dbd19197d3dd1b1c583b79028f07806ae25668f4
NONGFUER <13852291705@163.com>
2017-03-08 15:29:38 +0800

    增加不同尺寸的前缀类名

--- src/components/grid/Col.js
+++ src/components/grid/Col.js
@@ -34,13 +34,36 @@ export default class Col extends Component {
         const props = this.props;
         const {span,order,offset,push,pull,xs,sm,md,lg,className,children,prefixCls = 'tit-col', ...others} = props;
         let sizeClassObj = {};
+        const deviceSize = ['xs','sm','md','lg'];
+        deviceSize.forEach((size)=>{
+            let sizeProps ={
+                span: ' ',
+                order: ' ',
+                offset: ' ',
+                push: ' ',
+                pull: ' ',
+            };
+            if(typeof props[size] ==='number'){
+                    sizeProps.span = props[size];
+            }else if(typeof props[size] ==='object'){
+                sizeProps = props[size] || {};
+            }
+            delete others[size];
+            sizeClassObj = assign({},sizeClassObj,{
+                [`${prefixCls}-${size}-${sizeProps.span}`]: sizeProps.span !== undefined,
+                [`${prefixCls}-${size}-order-${sizeProps.order}`]: sizeProps.order || sizeProps.order === 0,
+                [`${prefixCls}-${size}-offset-${sizeProps.offset}`]: sizeProps.offset || sizeProps.offset === 0,
+                [`${prefixCls}-${size}-push-${sizeProps.push}`]: sizeProps.push || sizeProps.push === 0,
+                [`${prefixCls}-${size}-pull-${sizeProps.pull}`]: sizeProps.pull || sizeProps.pull === 0,
+            });
+        });
         const classes = classNames({
             [`${prefixCls}-${span}`]:span!== undefined,
             [`${prefixCls}-order-${order}`]:order,
             [`${prefixCls}-offset-${offset}`]:offset,
             [`${prefixCls}-push-${push}`]:push,
             [`${prefixCls}-pull-${pull}`]:pull
-        },className);
+        },className,sizeClassObj);
         return <div {...others} className={classes}>{children}</div>;
 
 
