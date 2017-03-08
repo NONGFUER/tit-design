dbd0209bd403497f3a4a189b0fbd9d762e7f0264
NONGFUER <13852291705@163.com>
2017-03-08 15:31:44 +0800

    增加grid路由

--- test/routes/index.js
+++ test/routes/index.js
@@ -4,11 +4,12 @@ import {Router,Route,IndexRoute,hashHistory} from 'react-router';
 
 import Frame from '../layouts/Frame'
 import Home from '../views/Home/Home'
+import GridDemo from '../views/Grid/GridDemo'
 const routes = (
         <Router history={hashHistory}>
             <Route  path='/' component={Frame}>
                 <IndexRoute component={Home} />
-            
+                <Route path="/grid" component={GridDemo}/>
             </Route>
         </Router>
 );
